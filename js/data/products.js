18f1a744e1ab729e979e516be92ee8f36c77165d
Yana <yanaegorova69.3@gmail.com>
2021-09-22 21:10:13 +0300

    +

--- js/data/products.js
+++ js/data/products.js
@@ -1,146 +1,152 @@
 export const products = [
     {
         id: 'product-1',
-        name: 'Soap Dish with Drain',
+        name: 'Swim Training Ear Plugs',
         price: 6.96,
         image: './img/img1.png',
         description: [
             {
-                title: 'Maximizes The Life Of Your Soap',
-                text: 'Slotted design can exactly let soap drain and dry naturally, which will preserve the life of your luxury soap. And the underneath container will keep back the water, not leaks onto the surface beneath it, keep your sink surface dry.',
+                text: 'The updated Ear Plugs are ergonomically designed to offer the closest most secure fit possible. Made of high quality TPR, these are super comfortable and super easy to place in or out of your ear.',
             },
             {
-                title: 'Made With Durable Plastic',
-                text: 'Even when dropped, they don\'t break or crack like the cheap plastic ones you can get from dollar stores.',
+                text: 'Ergonomic fit provides watertight comfort.',
             },
             {
-                title: 'Perfect Size',
-                text: 'Soap case size is 5.1" x 3.7" x 1.1", and the slotted part is high enough that soap does\'t sit in water and get mushy, extends the life of your soap. Perfect for bathroom and sink.',
+                text: 'Specialized shape minimizes leakage.',
             },
             {
-                title: 'Safe And Non-Toxic',
-                text: 'Our soap holder is zero volatile organic compounds (VOCs), which is important these days with an increasing number of people having allergies and sensitivities - our soap dish is not just a soap container but a health saver too.',
+                text: 'Soft and comfortable in PVC free material.',
             },
             {
-                title: 'Note',
-                text: 'No lid/cover for this soap container. This soap holder has two levels, the top container being removable, easy to take it apart to clean.',
+                text: 'Easy to put in and remove.',
+            },
+            {
+                text: 'Storage case included',
             },
         ],
-        images: ['./img/img1-1.png', './img/img1-2.png'],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png'],
     },
     {
         id: 'product-2',
-        name: 'Adult Swim Goggles',
+        name: 'Swim Training Fins',
         price: 29.99,
         image: './img/img2.png',
         description: [
             {
-                text: 'Comfortable flat strap with single point adjustment.',
+                text: 'Soft silicone short blade fins help to develop power and a stronger flutter kick.',
             },
             {
-                text: 'Unibody frame.',
+                text: 'Perfect for sprint sets, freestyle and backstroke.',
             },
             {
-                text: 'Anti fog, polycarbonate lenses.',
+                text: 'Soft silicone allows for more comfort and a secure fit.',
             },
         ],
         images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png'],
     },
     {
         id: 'product-3',
-        name: 'Baby Dome',
+        name: 'Big Foam Building Blocks',
         price: 64.90,
         image: './img/img3.png',
         description: [
             {
-                text: 'A play space and a comfy napping spot, perfect for when you and baby are on the go.',
+                title: 'Blocks Of Fun',
+                text: 'Constructed of dense foam ideal for soft play for children and toddlers.',
             },
             {
-                text: 'Can be used indoors or out. For babies 0-5 months.',
+                title: 'Activate Gross-Motor Skills',
+                text: 'Little ones develop important skills as they move and play at home, daycare or classrooms.',
             },
             {
-                text: 'Canopy protects your little one from the sun and bugs.',
+                title: 'Bright Colors',
+                text: 'Kids will learn more colors with the mix of bright colorful blocks for play time.',
             },
             {
-                text: '2 removable toys.',
+                title: 'Low Maintenance',
+                text: 'Colorful, soft materials are durable and easy to clean; just wipe with a mild soap-and-water solution.',
             },
             {
-                text: 'Folds flat, with handles for easy take-along.',
+                title: 'Safe For Your Space',
+                text: 'Certified and complies with CAL 117 flammability regulations.',
             },
         ],
-        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.png', './img/img3-5.png', './img/img3-6.png', './img/img3-7.png', './img/img3-8.png'],
+        images: ['./img/img3-1.png', './img/img3-2.png'],
     },
     {
         id: 'product-4',
-        name: 'Multi-Shelf',
+        name: 'Benchtop Drill Press',
         price: 98.90,
         image: './img/img4.png',
         description: [
             {
-                title: 'Modern And Eye-Catching Multi-Shelf',
-                text: 'This unique floating shelf adds a modern touch to complete your space while providing versatile shelving and storage.',
+                text: 'Features a 2.3A induction motor, an 8-inch swing, and a 2-inch spindle travel.',
             },
             {
-                title: 'Wall Decor & Storage',
-                text: 'Shelf includes two versatile and removable beech wood planters that can be used to show off your favorite greenery or store a variety of office or household items.',
+                text: 'Includes a 1/2-inch keyed chuck and onboard key storage.',
             },
             {
-                title: 'Customizable For Your Space',
-                text: 'Shelves can be moved around to fit your preference and measures 25 x 6. 44 x 19 inches (63. 5 x 16 x 48 cm).',
+                text: 'Operates at 5 different speeds: 740, 1100, 1530, 2100, or 3140 RPM.',
             },
             {
-                title: 'Easy To Set Up & Use',
-                text: 'Your floating shelf comes complete with simple step-by-step installation instructions and all the necessary hardware and brackets to easily mount it in just minutes.',
+                text: '6.5 by 6.5 inch height adjustable worktable bevels up to 45 degrees in each direction.',
+            },
+            {
+                text: 'Measures in at 23 inches tall with a 7 by 11-inch base.',
             },
         ],
-        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png', './img/img4-5.png'],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png'],
     },
     {
         id: 'product-5',
-        name: 'Storage Small Cabinet',
+        name: 'Propane Heater',
         price: 119.98,
         image: './img/img5.png',
         description: [
             {
-                text: 'Plastic.',
+                text: 'Outdoor gas heater with 46,000 BTUs, providing heat up to a 9-foot radius. Ideal for commercial cafes and restaurants or residential patios.',
             },
             {
-                text: 'Resin construction is durable and tough and virtually maintenance-free. Unlike wooden or metal, it will not rot, rust, dent, or peel.',
+                text: 'Made with aluminum, sturdy plastic, and rugged steel with a durable powder-coated finish that stands up to outdoor elements.',
             },
             {
-                text: 'Lockable doors keep valuables out of sight and hazardous chemicals safely away from children. (Lock not included).',
+                text: 'Piezo ignition system for easy push-button start ups; requires 20lb Liquid Propane Gas tank (not included); chain to help secure LPG tank provided.',
             },
             {
-                text: 'It has one ventilated shelf and one solid shelf. Shelving cannot be adjusted or removed.',
+                text: 'Safety auto shut-off tilt valve; waterbox for adding weight to the base; wheel assembly included for easy mobility.',
             },
             {
-                text: '13.5 cubic feet of storage space. Holds up to 336 pounds. Made in the USA.',
+                text: 'Secure the patio heater when not in use to avoid any potential damage to the patio heater.',
+            },
+            {
+                text: 'Measures approximately 18 x 33 x 89 inches (LxWxH); weighs 38 pounds.',
             },
         ],
-        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png'],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png', './img/img5-4.png'],
     },
     {
         id: 'product-6',
-        name: 'Speed Bag',
+        name: 'Glider and Ottoman',
         price: 148.00,
         image: './img/img6.png',
         description: [
             {
-                text: 'Height adjustable design for individual needs.',
+                title: 'Premium Comfort',
+                text: 'Offers you generous seating room, comfortable padded arm cushions with convenient storage pockets, and enclosed metal ball bearings for a smooth gliding motion to gently rock you and your little one.',
             },
             {
-                text: '24-inch diameter with 1.5 inch thick board for stabilization.',
+                title: 'Convenient Storage',
+                text: 'Each padded arm cushion includes a convenient storage pocket to hold any reading materials or essentials you need to keep within arm\'s reach.',
             },
             {
-                text: 'Professional grade design.',
+                title: 'Multiple Colors',
+                text: 'Designed to match any nursery, this glider is available in multiple cushion and base colors so it will match your style and blend seamlessly with the rest of your nursery furniture including including cribs, changing tables, dressers, mattresses, beds, and bunk beds.',
             },
             {
-                text: 'Kit includes: speed bag (10" x 7"), speed bag swivel and hand pump with needle.',
-            },
-            {
-                text: 'Wall mount hardware not included.',
+                title: 'Easy Clean',
+                text: 'The comfortable, polyester upholstery of our glider and ottoman set is spot cleanable, so you can keep your nursery looking fresh, clean and comfortable for you and your baby or infant. It\'s the ideal rocking chair for any stage of maternity.',
             },
         ],
-        images: ['./img/img6-1.png','./img/img6-2.png', './img/img6-3.png', './img/img6-4.png'],
+        images: ['./img/img6-1.jpeg','./img/img6-2.jpeg', './img/img6-3.jpeg', './img/img6-4.jpeg', './img/img6-5.jpeg', './img/img6-6.jpeg'],
     },
 ]
