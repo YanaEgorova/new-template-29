f11b527807793d2f06afdc9b28011c42de5e1815
Yana <yanaegorova69.3@gmail.com>
2021-09-22 18:35:52 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,146 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Soap Dish with Drain',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                title: 'Maximizes The Life Of Your Soap',
+                text: 'Slotted design can exactly let soap drain and dry naturally, which will preserve the life of your luxury soap. And the underneath container will keep back the water, not leaks onto the surface beneath it, keep your sink surface dry.',
+            },
+            {
+                title: 'Made With Durable Plastic',
+                text: 'Even when dropped, they don\'t break or crack like the cheap plastic ones you can get from dollar stores.',
+            },
+            {
+                title: 'Perfect Size',
+                text: 'Soap case size is 5.1" x 3.7" x 1.1", and the slotted part is high enough that soap does\'t sit in water and get mushy, extends the life of your soap. Perfect for bathroom and sink.',
+            },
+            {
+                title: 'Safe And Non-Toxic',
+                text: 'Our soap holder is zero volatile organic compounds (VOCs), which is important these days with an increasing number of people having allergies and sensitivities - our soap dish is not just a soap container but a health saver too.',
+            },
+            {
+                title: 'Note',
+                text: 'No lid/cover for this soap container. This soap holder has two levels, the top container being removable, easy to take it apart to clean.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Adult Swim Goggles',
+        price: 29.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Comfortable flat strap with single point adjustment.',
+            },
+            {
+                text: 'Unibody frame.',
+            },
+            {
+                text: 'Anti fog, polycarbonate lenses.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Baby Dome',
+        price: 64.90,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'A play space and a comfy napping spot, perfect for when you and baby are on the go.',
+            },
+            {
+                text: 'Can be used indoors or out. For babies 0-5 months.',
+            },
+            {
+                text: 'Canopy protects your little one from the sun and bugs.',
+            },
+            {
+                text: '2 removable toys.',
+            },
+            {
+                text: 'Folds flat, with handles for easy take-along.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.png', './img/img3-5.png', './img/img3-6.png', './img/img3-7.png', './img/img3-8.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Multi-Shelf',
+        price: 98.90,
+        image: './img/img4.png',
+        description: [
+            {
+                title: 'Modern And Eye-Catching Multi-Shelf',
+                text: 'This unique floating shelf adds a modern touch to complete your space while providing versatile shelving and storage.',
+            },
+            {
+                title: 'Wall Decor & Storage',
+                text: 'Shelf includes two versatile and removable beech wood planters that can be used to show off your favorite greenery or store a variety of office or household items.',
+            },
+            {
+                title: 'Customizable For Your Space',
+                text: 'Shelves can be moved around to fit your preference and measures 25 x 6. 44 x 19 inches (63. 5 x 16 x 48 cm).',
+            },
+            {
+                title: 'Easy To Set Up & Use',
+                text: 'Your floating shelf comes complete with simple step-by-step installation instructions and all the necessary hardware and brackets to easily mount it in just minutes.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png', './img/img4-5.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Storage Small Cabinet',
+        price: 119.98,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Plastic.',
+            },
+            {
+                text: 'Resin construction is durable and tough and virtually maintenance-free. Unlike wooden or metal, it will not rot, rust, dent, or peel.',
+            },
+            {
+                text: 'Lockable doors keep valuables out of sight and hazardous chemicals safely away from children. (Lock not included).',
+            },
+            {
+                text: 'It has one ventilated shelf and one solid shelf. Shelving cannot be adjusted or removed.',
+            },
+            {
+                text: '13.5 cubic feet of storage space. Holds up to 336 pounds. Made in the USA.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Speed Bag',
+        price: 148.00,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Height adjustable design for individual needs.',
+            },
+            {
+                text: '24-inch diameter with 1.5 inch thick board for stabilization.',
+            },
+            {
+                text: 'Professional grade design.',
+            },
+            {
+                text: 'Kit includes: speed bag (10" x 7"), speed bag swivel and hand pump with needle.',
+            },
+            {
+                text: 'Wall mount hardware not included.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png', './img/img6-3.png', './img/img6-4.png'],
+    },
+]
